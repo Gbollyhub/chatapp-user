66423eae31235c42dc5e69564a7ca13915ee4386
unknown <User01@LAPTOP-JMUHDCK3.signalalliance.com>
2020-07-12 17:43:29 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,301 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="wrapper">
+    <div class="home">
+    <h2>Chat App</h2>
+    <h5>Refres Browser to see new data added</h5>
+    <hr>
+    <br>
+
+    <div class="row">
+  <div class="col-1 pack">
+       <h4>Register</h4>
+    <form action="#">
+      <p style="color:green;font-size:11px;" v-show="userform">User Added Succesfully</p>
+     <input  type="text" placeholder="Name" v-model="user.name">
+     <br><br>
+      <input type="text" placeholder="Phone Number" v-model="user.phone">
+      <br><br>
+      <button @click="register" class="button">Add User</button>
+    </form>
+    </div>
+
+      <div class="col-2 pack">
+       <h4>All User</h4>
+    <div v-for="user in allusers" :key="user.id" class="user-box">
+    {{ user.id }} | {{ user.name }} | {{ user.phone }}
+    </div>
+    </div>
+    </div>
+
+        <div class="row">
+  <div class="col-1 pack">
+       <h4>Login </h4>
+        <p style="color:green;font-size:11px;" v-show="loginform">Login Successfully</p>
+    <form action="#">
+     <input  type="text" placeholder="Your Id" v-model="loginuser.id">
+     <br><br>
+      <button @click="loginAndGetUsers" class="button">Login & fetch Users</button>
+    </form>
+    <h4>Add a Friend</h4>
+          <p style="color:green;font-size:11px;" v-show="adduserform">Your friend has been addded</p>
+        <form action="#">
+          <input  type="text" placeholder="Adder Id" v-model="adderuser.id">
+     <br><br>
+     <input  type="text" placeholder="Friend id" v-model="frienduser.id">
+     <br><br>
+      <button @click="addAFriend" class="button">Add Friend</button>
+    </form>
+    <br>
+    </div>
+
+           <div class="col-2 pack">
+            <h4 v-if="userDetails.name != null"> {{userDetails.name}}({{usersFriends.length}})</h4>
+       <h4 v-else>Login to show your friends</h4>
+       <div v-if="usersFriends.length > 0">
+         <div v-for="user in usersFriends" :key="user.id" class="user-box">
+      {{ user.id }} | {{ user.name }} | {{ user.phone }}
+    </div>  
+       </div>
+    <div v-else class="user-box">
+        You dont have any Contact. Add a Friend 
+       </div>
+    </div>
+    </div>
+  
+   
+  
   </div>
+</div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import db from './firebaseinit.js';
+import firebase from 'firebase';
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      user: {
+        id: '',
+        name: "",
+        phone: ""
+      },
+
+      loginuser: {
+        id: '',
+        name: "",
+        phone: ""
+      },
+        adderuser:{
+           id: ''
+        },
+      frienduser: {
+        id: ''
+      },
+      userform: false,
+      loginform: false,
+      adduserform: false,
+
+      allusers:[],
+
+      userDetails: {
+         id: null,
+        name: null,
+        phone: null
+      },
+
+        adderresult: {
+         id: null,
+        name: null,
+        phone: null
+      },
+      
+       addfriendresult: {
+         id: null,
+        name: null,
+        phone: null
+      },
+
+      usersFriends:[]
+
+    }
+  },
+  created(){
+                   db.collection('users').get().then(
+        querySnapshot => {
+          querySnapshot.forEach(doc =>{
+           const data  = {
+                 'id' : doc.data().UserId,
+                 'name' : doc.data().Fullname,
+              'phone' : doc.data().PhoneNumber,
+           }
+           this.allusers.push(data)
+          })
+        } 
+      )
+  },
+  methods:{
+
+      generateUserId(){
+            var text = "";
+     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+   
+     for (var i = 0; i < 8; i++)
+       text += possible.charAt(Math.floor(Math.random() * possible.length));
+       this.user.id = text;
+        },
+
+        register(e){
+            e.preventDefault();
+           this.generateUserId()
+            db.collection('users').doc(this.user.id).set({
+                UserId: this.user.id,
+                Fullname: this.user.name,
+                PhoneNumber: this.user.phone
+             }).then(()=>{ this.userform = true }).catch(error => alert(error)) 
+        },
+
+        loginAndGetUsers(e){
+             e.preventDefault();
+             this.usersFriends = [];
+           db.collection('users').doc(this.loginuser.id).get().then(
+       (doc) => {
+                     if (doc.exists) {
+        console.log("Document data:", doc.data());
+          this.userDetails.id = doc.data().UserId
+            this.userDetails.name = doc.data().Fullname
+            this.userDetails.phone = doc.data().PhoneNumber
+
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+          }).then(()=>{
+                   db.collection('users').doc(this.loginuser.id).collection('contactList').get().then(
+        querySnapshot => {
+          querySnapshot.forEach(doc =>{
+           const data  = {
+                 'id' : doc.data().UserId,
+                 'name' : doc.data().Fullname,
+              'phone' : doc.data().PhoneNumber,
+           }
+           this.usersFriends.push(data)
+          })
+        } 
+      )
+      }).then(()=>{ this.loginform = true }).catch(error => alert(error)) 
+      
+        },
+       async addAFriend(e){
+          e.preventDefault();
+         console.log(this.frienduser.id)
+
+          const isFriendAUser = await db.collection('users').doc(this.frienduser.id).get()
+
+            if(isFriendAUser.exists){
+        const getUser = await db.collection('users').doc(this.adderuser.id).collection('contactList').doc(this.frienduser.id).get()
+  if( getUser.exists){
+       alert("User Exists as your friend already")
   }
+  else{
+           db.collection('users').where('UserId', '==', this.frienduser.id).get().then(
+            querySnapshot => {
+              querySnapshot.forEach(doc =>{
+                    this.addfriendresult.id = doc.data().UserId
+                this.addfriendresult.name = doc.data().Fullname
+                this.addfriendresult.phone = doc.data().PhoneNumber
+              })
+            }
+          ).then(()=>{
+                 console.log("gotten Friend" + this.addfriendresult.name)
+                  db.collection('users').where('UserId', '==', this.adderuser.id).get().then(
+            querySnapshot => {
+              querySnapshot.forEach(doc =>{
+                    this.adderresult.id = doc.data().UserId
+                this.adderresult.name = doc.data().Fullname
+                this.adderresult.phone = doc.data().PhoneNumber
+              })
+            }
+          ).then(()=>{
+            console.log("getten Adder" + this.adderresult.name)
+              db.collection('users').doc(this.adderuser.id).collection('contactList').doc(this.addfriendresult.id).set({
+                     UserId: this.addfriendresult.id,
+                    Fullname: this.addfriendresult.name,
+                    PhoneNumber: this.addfriendresult.phone
+             })
+          }).then(()=>{
+             console.log("Added Adder"+ this.addfriendresult.name)
+             db.collection('users').doc(this.frienduser.id).collection('contactList').doc(this.adderresult.id).set({
+                     UserId: this.adderresult.id,
+                    Fullname: this.adderresult.name,
+                    PhoneNumber: this.adderresult.phone
+             })
+          })
+          }).then(()=>{ this.adduserform = true }).catch(error => console.log(error)) 
+      alert("New Friend Added")
+  }
+         }
+  else{
+   alert("This User does not in the system")
+  }
+        }
+
+
+        }
+  
 }
 </script>
+
+<style scoped>
+.user-box{
+      border: 1px solid #f0f0f0;
+  border-radius: 3px;
+  padding: 5px;
+  font-size:12px;
+  margin-bottom: 10px;
+}
+.row{
+  margin: 0 auto;
+  width: 100%;
+  margin-bottom: 30px;
+}
+    .row::after {
+      content: "";
+    display: table;
+    clear: both;  
+    } 
+    .col-1{
+        float: left;
+        width:45%;
+        margin-right: 2%;
+        margin-bottom: 20px;
+    }
+    .col-2{
+        float: left;
+        width:45%;
+        margin-bottom: 20px;
+    }
+.pack{
+    border: 1px solid #f0f0f0;
+  border-radius: 10px;
+  padding: 10px;
+}
+input{ 
+  height: 30px;
+  padding-left: 10px;
+}
+button{
+  background: rgb(27, 95, 231);
+  color: white;
+  padding: 10px;
+}
+.wrapper{
+  padding: 50px 100px 0 100px;
+}
+.home{
+  border: 1px solid #f0f0f0;
+  border-radius: 10px;
+  padding: 30px;
+}
+</style>
---
+++ src/views/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyAzv3sUXc_XV_KA6VSy8fmtBN0XYQ9jf5Y",
+    authDomain: "spark-1f7bd.firebaseapp.com",
+    databaseURL: "https://spark-1f7bd.firebaseio.com",
+    projectId: "spark-1f7bd",
+    storageBucket: "spark-1f7bd.appspot.com",
+    messagingSenderId: "18658625969",
+    appId: "1:18658625969:web:1af63d4e4b813b145ed590"
+    }
